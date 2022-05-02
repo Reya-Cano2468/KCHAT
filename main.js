15a30f4a6c70ed5847a237459b4bff0ebc24cdae
Reya-Cano2468 <74850456+Reya-Cano2468@users.noreply.github.com>
2022-05-02 13:53:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function bedroom(){
+    window.location.replace("bedroom.html");
+}
+
+function kitchen(){
+    window.location.replace("kitchen.html");
+}
+
+function hall(){
+    window.location.replace("hall.html");
+}
+
+function garden(){
+    window.location.replace("garden.html");
+}
